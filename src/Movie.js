b5a7ffea585f5e49b9f6494e2ca028cad6f42f6f
Unknown <s0567742@htw-berlin.de>
2018-11-28 10:37:15 +0100

    Adjust movie overlays

--- src/Movie.js
+++ src/Movie.js
@@ -14,7 +14,7 @@ export default function Movie(props) {
             <div className="overlay">
                 <div className="rating"> {/*include rating */} </div>
                 <h5 className="genre">International</h5>
-                <h2 className="title">{props.movies.title}</h2>
+                <h2 className="title max-lines">{props.movies.title}</h2>
                 <div className="release-date"><h4>{props.movies.release_date}</h4></div>
             </div>
         </div>
