ff14047392f84582c7770f1132f0a28f9451d875
Unknown <s0567742@htw-berlin.de>
2018-12-03 11:33:15 +0100

    Increase number of rating stars and adapt style accordingly

--- src/Components/Rating.js
+++ src/Components/Rating.js
@@ -14,6 +14,16 @@ export default function Rating() {
 				<label className="star-4" htmlFor="star-4">4</label>
 				<input type="radio" name="star" className="star-5" id="star-5" />
 				<label className="star-5" htmlFor="star-5">5</label>
+				<input type="radio" name="star" className="star-6" id="star-6" />
+				<label className="star-6" htmlFor="star-6">6</label>
+				<input type="radio" name="star" className="star-7" id="star-7" />
+				<label className="star-7" htmlFor="star-7">7</label>
+				<input type="radio" name="star" className="star-8" id="star-8" />
+				<label className="star-8" htmlFor="star-8">8</label>
+				<input type="radio" name="star" className="star-9" id="star-9" />
+				<label className="star-9" htmlFor="star-9">9</label>
+				<input type="radio" name="star" className="star-10" id="star-10" />
+				<label className="star-10" htmlFor="star-10">10</label>
 				<span></span>
 			</div>
 		</form>
