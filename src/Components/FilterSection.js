94f4c847fac35d1cc7784ff111bca2d3223fe461
Unknown <s0567742@htw-berlin.de>
2018-12-03 13:42:37 +0100

    Join ButtonList and FilterSection

--- src/Components/FilterSection.js
+++ src/Components/FilterSection.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Button from './Button';
 import Rating from './Rating';
+import genreJson from "../dummy-json-responses/Genres";
 
-export default function FilterSection() {
-	return (
-		<div className="filter-section">
-			<Button title="Actor"/>
-			<Button title="Genre"/>
-			<Button title="Timeframe"/>
-			<div className="rating-column">
-				<h3>Rating</h3>
-				<div className="rating-filter">
-					<Rating/>
-				</div>
-			</div>
-		</div>
-	);
+class FilterSection extends Component {
+
+	getGenres() {
+		 const dataString = JSON.stringify(genreJson);
+		 let jsonData = JSON.parse(dataString);
+		 let genreArray = jsonData.genre;
+		 let content = [];
+
+		 for (let i = 0; i < genreArray.length; i++) {
+				 content.push(genreArray[i]);
+		 }
+		 return content;
+ }
+     render() {
+				return (
+					<div className="filter-section">
+						<Button title="Actor" select="select actor"/>
+						<Button title="Genre" select="select genre" content={this.getGenres()}/>
+						<Button title="Timeframe" select="select timeframe"/>
+						<div className="rating-column">
+							<h3>Rating</h3>
+							<div className="rating-filter">
+								<Rating/>
+							</div>
+						</div>
+					</div>
+				);
+			}
 }
+export default FilterSection;
