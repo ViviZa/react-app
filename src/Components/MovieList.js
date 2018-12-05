c573573c3e319550aa7de42cc50947ec3f7dd584
Vivian Zaddach <vivian.zaddach@idealo.de>
2018-12-05 14:27:49 +0100

    serialize json response

--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 import jsondata from "../dummy-json-responses/imdb_data";
 import Movie from "./Movie";
-import parseMovie, {getMovies} from "./ParseJson";
+import parseMovie, {getMovies, parseMovies2} from "./ParseJson";
 
 class MovieList extends Component {
 
     constructor() {
         super();
-        this.state = {movies: getMovies()}
+        this.state = {movies: parseMovies2()}
     }
 
     render() {
--- src/Components/ParseJson.js
+++ src/Components/ParseJson.js
@@ -15,6 +15,20 @@ export default parseMovies;
 
 
 
+export const parseMovies2 = () => {
+    const dataString = JSON.stringify(getMovies());
+    let jsonData = JSON.parse(dataString);
+    let movies = [];
+
+    for (let i = 0; i < jsonData.length; i++) {
+        movies.push(jsonData[i]);
+    }
+    return movies;
+};
+
+export default parseMovies2;
+
+
 export const getMovies = () => {
     fetch('http://localhost:5000/getAllMovies').then(function (response) {
         return response;
