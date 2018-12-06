852bd8eaa32d3f128f2189bb49f80624b087a223
Unknown <s0567742@htw-berlin.de>
2018-12-06 13:45:50 +0100

    Add reset button styling

--- src/Components/DropDown.js
+++ src/Components/DropDown.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
+import { faAngleDown, faUndo } from '@fortawesome/free-solid-svg-icons'
 
 library.add(faAngleDown)
+library.add(faUndo)
 
 class DropDown extends Component {
 
@@ -23,16 +24,18 @@ class DropDown extends Component {
     render() {
         return (
             <div className="filter-column">
-                <div className="dropdown">
-                    <h3>{this.state.title}</h3>
-                    <button className="reset-button" onClick={this.handleClick.bind(this, "select")}>x</button>
-                    <div className="dropdown">
-                        <button className="dropbtn">{this.state.select}<span className="arrow-down"><FontAwesomeIcon icon="angle-down" /></span></button>
-                        <div className="dropdown-content">
-                            {this.state.content.map((choice,i) => <a key={i} onClick={this.handleClick.bind(this, choice)}>{choice}</a>)}
-                        </div>
-                    </div>
-                </div>
+              <div className="filter-headline">
+                <h3>{this.state.title}</h3>
+                <button className="reset-button" onClick={this.handleClick.bind(this, "select")}>
+                  <span className="reset-filter"><FontAwesomeIcon icon="undo" /></span>
+                </button>
+              </div>
+              <div className="dropdown">
+                  <button className="dropbtn">{this.state.select}<span className="arrow-down"><FontAwesomeIcon icon="angle-down" /></span></button>
+                  <div className="dropdown-content">
+                      {this.state.content.map((choice,i) => <a key={i} onClick={this.handleClick.bind(this, choice)}>{choice}</a>)}
+                  </div>
+              </div>
             </div>
         );
     }
