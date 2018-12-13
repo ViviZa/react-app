ddef2b27cfa0ec68456f013dd20a1a0450ae74b3
Vivian Zaddach <vivian.zaddach@idealo.de>
2018-12-13 14:01:48 +0100

    implement reset button, which resets the filtered App to the initial state of movies in database

--- src/Components/App.js
+++ src/Components/App.js
@@ -10,20 +10,33 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.onDropDownClick = this.onDropDownClick.bind(this);
+        this.onResetClick = this.onResetClick.bind(this);
+        this.onRenderCurrentMovies = this.onRenderCurrentMovies.bind(this);
         this.state = { movies: [] }
     }
 
     componentWillMount() {
+        this.onRenderCurrentMovies();
+    }
+
+    onRenderCurrentMovies(){
         fetch('http://127.0.0.1:5000/getAllMovies')
             .then(response => response.json())
             .then(json => parseMovies(json))
             .then(data => this.setState({ movies: data }));
-            // für lokales arbeiten: this.state.movies = parseMovies(jsondata);
+        // für lokales arbeiten:
+        // this.state.movies = parseMovies(jsondata);
     }
 
     onDropDownClick(){
-        //clears movies -> later it will call the backend by wanted search criteria
+        //TODO: fetch backend by filter criteria
+        //now it just clears movies
         this.state.movies = [];
+        this.forceUpdate();
+    }
+
+    onResetClick(){
+        this.onRenderCurrentMovies();
         this.forceUpdate();
     }
 
@@ -32,6 +45,7 @@ class App extends Component {
             <div className="container">
                 <Header/>
                 <FilterSection renderApp={this.onDropDownClick}/>
+                <button onClick={this.onResetClick}>Reset</button>
                 <MovieList movies={this.state.movies}/>
             </div>
         );
