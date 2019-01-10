15f393a75b2210833440a1c67259a8f2aeb8908f
Lina <lina.kriebel@me.com>
2019-01-10 16:01:17 +0100

    fetch filtered movies

--- src/Components/App.js
+++ src/Components/App.js
@@ -10,14 +10,12 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.onDropDownClick = this.onDropDownClick.bind(this);
-        this.getMovies = this.getMovies.bind(this);
         this.state = {
-            movies: [],
+            movies: ['initialState'],
             genres: [],
             actors: [],
-            noBackEndResponse: false,
-            isLoading: false
-        }
+            noBackEndResponse: false
+         }
     }
 
     componentWillMount() {
@@ -27,9 +25,7 @@ class App extends Component {
     }
 
     getMovies() {
-        this.setState({isLoading: true});
-
-        fetch('http://127.0.0.1:5000/getAllMovies')
+        fetch('http://127.0.0.1:5000/getMovies')
             .then(response => response.json())
             .then(json => parseData(json))
             .then(data => this.setState({movies: data, isLoading: false}))
@@ -51,30 +47,30 @@ class App extends Component {
     }
 
     onDropDownClick(actor, genre, rating) {
-        //TODO: fetch backend by filter criteria
-        console.log("actor " + actor + "..." + "genre " + genre + "..." + "rating" + rating);
-        //now it just clears movies
-        this.state.movies = [];
-        this.forceUpdate();
+        fetch('http://127.0.0.1:5000/getMovies?actor=' + actor + '&genre=' + genre + '&rating=' +rating)
+            .then(response => response.json())
+            .then(json => parseData(json))
+            .then(data => this.setState({movies: data}));
     }
 
     render() {
         let movies;
         const movieState = this.state.movies;
 
-        if (this.state.isLoading) movies = <MovieList movies=""/>;
-
-        if (this.state.noBackEndResponse) {
-            //if back-end does not response
-            movies = <div className="no-movies"><NotFound/><p>No movies available.</p></div>;
-        } else if (movieState.length !== 0){
+        if (movieState[0] == 'initialState'){
+            //if movies are still fetched
+            movies = <p>is Loading...</p>
+       } else if (movieState.length !== 0){
             //if back-end did response with movies
             movies = <MovieList movies={this.state.movies}/>;
         } else {
             //if back-end did response with no movies
             movies = <div className="no-movies"><NotFound/><p>No movies with selected criteria available.</p></div>;
         }
-
+        if (this.state.noBackEndResponse) {
+            //if back-end does not response
+            movies = <div className="no-movies"><NotFound/><p>Currently no movies available.</p></div>;
+        }
         return (
             <div className="container">
                 <Header/>
