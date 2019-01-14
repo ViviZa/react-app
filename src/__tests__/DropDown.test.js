3e399e11b37ab00fec556dbb107d134cc9922382
Vivian Zaddach <vivian.zaddach@idealo.de>
2019-01-14 11:29:00 +0100

    writing jest tests with enzyme is now possible

--- src/__tests__/DropDown.test.js
+++ src/__tests__/DropDown.test.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import {configure, mount} from 'enzyme';
 import DropDown from "../Components/DropDown";
 import actorJson from "../dummy-json-responses/Actors";
+import Adapter from "enzyme-adapter-react-16";
+
+
+configure({ adapter: new Adapter() });
+
 
 test('TodoComponent renders the text inside it', () => {
+    ///shallow um die Komponente ohne "Unterkomponenten" zu rendern
     const wrapper = mount(
-        <DropDown title="Actor" type="actor" content={actorJson} onDropDownClick={this.onDropDownClick}/>
+        <DropDown title="Actor" type="actor" content={actorJson} onDropDownClick={() => console.log("")}/>
     );
     const p = wrapper.find('.toggle-todo');
     expect(p.text()).toBe('Buy Milk');
